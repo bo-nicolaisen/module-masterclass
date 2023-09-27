3f7fc088dd13820cdf7f95bbbe2d5052d667b9eb
bo-nicolaisen <bni@techcollege.dk>
2023-09-27 15:33:05 +0200

    initial commit

---
+++ assets/js/modules/views.js
@@ -0,0 +1,9 @@
+
+export function showUsers(userData) {
+    console.info(userData);
+}
+
+export function showSingleUser(myUser) {
+    console.log(myUser);
+
+}
--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,45 @@
-function
+import { showUsers, showSingleUser } from "./modules/views.js";
+
+const myApp = document.getElementById('app');
+
+
+// model code ------------------------------------------------------------
+
+let myUserData
+
+initApp()
+
+function initApp() {
+    getUsers();
+}
+
+// fetch
+export function getUsers() {
+
+    fetch('https://dummyjson.com/users')
+        .then((res) => {
+            return res.json();
+        })
+        .then((res) => {
+            dataconversion(res);
+        })
+}
+
+//callback
+
+function dataconversion(rawData) {
+    myUserData = rawData.users;
+    showUsers(myUserData);
+}
+
+
+// view code ------------------------------------------------------------
+
+/* function showUsers(userData) {
+    console.info(userData);
+}
+
+function showSingleUser(myUser) {
+    console.log(myUser);
+
+} */
